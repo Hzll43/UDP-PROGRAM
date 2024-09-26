8b79061ae0ac30e464b3093832f8b8a3d26f6654
Hzll43 <gibrannaro8@gmail.com>
2024-09-26 18:27:15 +0700

    Update host_tes.py

--- host_tes.py
+++ host_tes.py
@@ -1,18 +1,15 @@
 import socket
 
-# Server IP address (set to '0.0.0.0' to accept connections from any IP)
-server_ip = '0.0.0.0'  
+server_ip = '0.0.0.0'  # Server IP address (set to '0.0.0.0' to accept connections from any IP)
 server_port = 8080    #adjust the port of the host and client
 
-# Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP/IP socket
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
     sock.bind((server_ip, server_port))
 
-    # Start listening for incoming connections
-    sock.listen(5)
+    sock.listen(5)   # Start listening for incoming connections 
     print(f"Server listening on {server_ip}:{server_port}...")
 
     while True:
@@ -24,8 +21,6 @@ server_port = 8080    #adjust the port of the host and client
         data = client_socket.recv(1024).decode('utf-8')
         print(f"Received from client: {data}")
     
-        # Send a response back to the client
-        client_socket.send(b"Hello from server!")
+        client_socket.send(b"Hello from server!")  # Send a response back to the client
     
-        # Close the client socket
-        client_socket.close()
+        client_socket.close()     # Close the client socket
